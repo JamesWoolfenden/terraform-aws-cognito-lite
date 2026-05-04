a02112a965ec380388113d7e01f7af5f0659f2c3
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 23:22:48 +0100

    ci: apply pre-commit auto-fixes

--- example/examplea/outputs.tf
+++ example/examplea/outputs.tf
@@ -1,11 +1,9 @@
 output "domain" {
   value = module.cognito.domain
 }
-
 output "user_pool" {
   value = module.cognito.user_pool
 }
-
 output "user_pool_client" {
   value     = module.cognito.user_pool_client
   sensitive = true
